6461de19f35d54af6348db7354df139975b2c9b4
techylimat <151425937+techylimat@users.noreply.github.com>
2025-08-22 00:39:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,10 @@ llm = HuggingFaceHub(
 )
 
 
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    model_kwargs={"device": "cpu"}
+)
 
 # --- Build vectorstore
 vs = None
