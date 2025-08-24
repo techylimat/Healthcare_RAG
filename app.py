77bb539dd9820026a1a72b40f9c9f700cf4ece85
techylimat <151425937+techylimat@users.noreply.github.com>
2025-08-24 23:44:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ llm = HuggingFaceHub(
 )
 
 
-  embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 
 
